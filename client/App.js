cfbb2be29a1b19527a21ca1c0f0e9b5f9fbde4de
Sayyed Sohel Ahmed <sayyedsohel73976@gmail.com>
2023-09-13 19:47:19 +0530

    add twrnc

--- client/App.js
+++ client/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import { View, Text ,TouchableOpacity} from 'react-native';
+import {StatusBar} from "expo-status-bar"
+import tw from 'twrnc';
 import { NavigationContainer } from "@react-navigation/native";
 
-import AppNavigator from "./src/Navigation/AppNavigator";
+// import AppNavigator from "./src/Navigation/AppNavigator";
 
 function App() {
   return (
     <NavigationContainer>
-      <AppNavigator />
+      <View style={tw`flex-1 justify-center items-center bg-black`}>
+        <TouchableOpacity style={tw`bg-teal-500 p-3 rounded-lg `}>
+        <Text style={tw`text-white text-3xl font-bold`}>Hello World</Text>
+        </TouchableOpacity>
+        <StatusBar style={'light'} />
+         
+      </View>
+      {/* <AppNavigator /> */}
     </NavigationContainer>
   );
 }
